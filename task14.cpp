e64138c8d1c749cbb957ff589a20ba24a93ad642
yash <rajeshbhaiyash248@gmail.com>
2023-06-09 18:06:29 +0530

    hello

---
+++ task14.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class skill
+{
+    int l,w,h;
+    public:
+    void setdata(int a,int b,int c)
+    {
+        l=a ,w=b ,h=c;
+    }
+    int getdata()
+    {
+        return l*w*h;
+    }
+    skill operator+(skill n)
+    {
+        skill t;
+        t.l=l+n.l;
+        t.w=w+n.w;
+        t.h=h+n.h;
+        return t;
+    }
+};
+int main()
+{
+    skill a,b,c;
+    a.setdata(2,2,2);
+    cout<<"volume of skill A is: "<<a.getdata()<<endl;
+
+    b.setdata(3,3,3);
+    cout<<"volume of skill B is: "<<b.getdata()<<endl;
+
+    c=a+b;
+    cout<<"volume of skill C is: "<<c.getdata()<<endl;
+}
